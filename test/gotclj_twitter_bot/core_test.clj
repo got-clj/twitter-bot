478189eb793fd5fa605360ac4c565a6741b48231
Patrik Kårlin <patrik.karlin@gmail.com>
2013-10-29 17:33:22 +0100

    Init lein project

---
+++ test/gotclj_twitter_bot/core_test.clj
@@ -0,0 +1,7 @@
+(ns gotclj-twitter-bot.core-test
+  (:require [clojure.test :refer :all]
+            [gotclj-twitter-bot.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
