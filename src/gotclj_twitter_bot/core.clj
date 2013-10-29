49950b9282ecf9643eaba4da27fcbd0dfbf9e01e
Jell <jean-louis@jawaninja.com>
2013-10-29 17:55:53 +0100

    Added twitter library

--- src/gotclj_twitter_bot/core.clj
+++ src/gotclj_twitter_bot/core.clj
@@ -1,6 +1,20 @@
-(ns gotclj-twitter-bot.core)
+(ns gotclj-twitter-bot.core
+  (:use
+   [twitter.oauth]
+   [twitter.callbacks]
+   [twitter.callbacks.handlers]
+   [twitter.api.restful]))
+  (:import
+   (twitter.callbacks.protocols SyncSingleCallback))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def *app-consumer-key* "")
+(def *app-consumer-secret* "")
+(def *user-access-token* "")
+(def *user-access-token-secret* "")
+
+(def twitter-creds (make-oauth-creds *app-consumer-key*
+                                     *app-consumer-secret*
+                                     *user-access-token*
+                                     *user-access-token-secret*))
+
+(users-show :oauth-creds twitter-creds :params {:screen-name "Gotclj"})
